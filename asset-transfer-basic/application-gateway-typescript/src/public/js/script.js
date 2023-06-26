3ed22d3ee54e14cb75b4a1b3ef88ba5e225d6a26
thiagomartendal <thiagomartendal@gmail.com>
2023-06-26 11:12:36 -0300

    Add files via upload

---
+++ asset-transfer-basic/application-gateway-typescript/src/public/js/script.js
@@ -0,0 +1,95 @@
+function viewContract() {
+    let view_contract = document.getElementById('view_contract')
+    let create_contract = document.getElementById('create_contract')
+    view_contract.style.display = 'block';
+    create_contract.style.display = 'none';
+}
+
+function createContract() {
+    let view_contract = document.getElementById('view_contract')
+    let create_contract = document.getElementById('create_contract')
+    view_contract.style.display = 'none';
+    create_contract.style.display = 'flex';
+}
+
+function displayLightbox(id, amount, status) {
+    let contract_id = document.getElementById('contract_id')
+    let contract_amout = document.getElementById('contract_amout')
+    let contract_status = document.getElementById('contract_status')
+    let lightbox = document.getElementById('lightbox')
+    contract_id.textContent = id
+    contract_amout.textContent = amount
+    contract_status.textContent = status
+    lightbox.classList.add('show')
+}
+
+function closeLightbox() {
+    let lightbox = document.getElementById('lightbox')
+    lightbox.classList.remove('show')
+}
+
+async function getTransactions() {
+	const response = await fetch('/transactions')
+	const jsonData = await response.json()
+	//console.log(jsonData)
+	let list_contracts = document.getElementById('list_contracts')
+	list_contracts.innerHTML = ''
+	jsonData.forEach(item => {
+		if (item.docType == 'contract') {
+			console.log('ITEM: ', item)
+			list_contracts.innerHTML += `<h5 onclick="displayLightbox('${item.ID}', '${item.Amount}', '${item.Status}')">Contract ${item.ID}</h5>`
+		}
+	})
+}
+
+async function sendContract() {
+	let signature1 = document.querySelector('input[name="contractor"]').value
+	let signature2 = document.querySelector('input[name="hired"]').value
+	let amount = document.querySelector('input[name="amount"]').value
+	await fetch('/create-escrow-contract', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({
+			client: signature1,
+			freelancer: signature2,
+			value: amount
+		})
+	})
+	await viewContract()
+	await getTransactions()
+	await document.getElementById('new_contract').reset()
+}
+
+async function updateContract() {
+	let signature = document.querySelector('input[name="confirmation_signature"]').value
+	let operation = document.querySelector('input[name="operation_contract"]:checked').value
+	let contract_id = document.getElementById('contract_id').textContent
+	console.log(operation)
+	let direction = ''
+	let json = ''
+	if (operation == 'confirm') {
+		direction = '/approve-work'
+		json = JSON.stringify({
+			contractId: contract_id,
+			approver: signature
+		})
+	} else {
+		direction = '/cancel-contract'
+		json = JSON.stringify({
+			contractId: contract_id,
+			canceller: signature
+		})
+	}
+	console.log(direction)
+	await fetch(direction, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: json
+	})
+	await closeLightbox()
+	window.location.href = '/'
+}
